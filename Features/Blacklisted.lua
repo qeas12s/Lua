e6e59fcb481aa0521a2cc3a911890de7419e43e8
Space <94127295+SpaceYes@users.noreply.github.com>
2022-06-12 21:52:06 +0800

    Update Blacklisted.lua

--- Features/Blacklisted.lua
+++ Features/Blacklisted.lua
@@ -1,6 +1,9 @@
 if game:GetService('Players').LocalPlayer.UserId == 2458121333 then
-      game:GetService('Players').LocalPlayer:Kick('RAYX BANNED : You were blacklisted because you are recreating rayx gui, the content contained copyrighted material owned by Space. COPYRIGHTS / DMCA / LAWSUIT')
+      game:GetService('Players').LocalPlayer:Kick('\n RAYX BANNED : \n You were blacklisted because you are recreating rayx gui, the content contained copyrighted material owned by Space. COPYRIGHTS / DMCA / LAWSUIT')
 end
 if game:GetService('Players').LocalPlayer.UserId == 2494832180 then
-      game:GetService('Players').LocalPlayer:Kick('RAYX BANNED : Wont leave premium users alone')
+      game:GetService('Players').LocalPlayer:Kick('\n RAYX BANNED : \n Wont leave premium users alone')
+end
+if game:GetService('Players').LocalPlayer.UserId == 1629813740 then
+      game:GetService('Players').LocalPlayer:Kick('\n RAYX BANNED : \n Dude are you good? "scammer get scammed" 🤡')
 end
